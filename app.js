d59439591e61b8c8868971240a3b8d28cb946c92
Dmitry <lupanov5@mail.ru>
2021-09-12 13:41:50 +0300

    унифицировал функцию галереи

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide')
+function cardsGalery(slideSelector, activeClass, event, activeSlide = 0) {
+    const slides = document.querySelectorAll(slideSelector)
 
-slides.forEach((slide) => {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')        
-    })
-})
+    slides[activeSlide].classList.add(activeClass)
 
-function clearActiveClasses() {
     slides.forEach((slide) => {
-        slide.classList.remove('active')
+        slide.addEventListener(event, () => {
+            clearActiveClasses()
+            slide.classList.add(activeClass)        
+        })
     })
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove(activeClass)
+        })
+    }
 }
+
+cardsGalery('.slide', 'active', 'mouseover')
